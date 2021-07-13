0cd2f8afe809195bfce58968e4c7cdfce108fb2c
HACKDRES OFICIAL <87175555+HACKDRES2@users.noreply.github.com>
2021-07-13 17:13:31 -0500

    Add files via upload

---
+++ Host.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#credite by: hackdres oficial
+
+echo "¿Mi nombre es?: "
+read es
+echo "¿Mi edad es?: "
+read edad
+echo "¿Estoy suscrito al canal hackdres oficial?: "
+read Estoy suscrito al canal hackdres oficial
+
+#suscribete al canal hackdres oficial
+
+echo "Okey $es Lo es que are es mostrarte un status de un host"
+sleep 1
+echo "ingresar host: "
+read host
+sleep 1
+echo "buscando status y abriendo los comandos de instalacion"
+sleep 1
+pkg install curl
+curl -v $host
+sleep 1
+echo -e $color
+echo " .                                                      ."
+echo "      .n                     .             .                n."
+echo "  . .dP                   dP               9b               9b   ."
+echo " 4  qXb         .        dX                 Xb       .      dXp   t"
+echo "dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb"
+echo "9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP"
+echo " 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP"
+echo "   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP "
+echo -e "  9XXXXXP   9XX             $r*$g           98v8P      $r*$g     XXP   9XXXXXXXP "
+echo "      ~~~       9X.          .db|db.          .XP       ~~~"
+echo "                  )b.  .dbo.dP' v  9b.odb.  .dX("
+echo "                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb."
+echo "               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb"
+echo "              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb"
+echo "              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP"
+echo "                       9XXXXXX(   )XXXXXXP        "
+echo "                       XXXX X. v .X XXXX"
+echo "                        XP^X' b   d' X^XX"
+echo "                        X. 9         P )X"
+echo "                        b          '  d'"
+sleep 1.5
+                         echo -e "Listo a terminado su busqueda"
+                         sleep 1
+
+                         echo "visita mi canal de youtube (hackdres oficial)"
+                         sleep 1
+
+                         echo "https://youtube.com/channel/UCTSAjBuSJiqSYXd_dqg4qBg"
+#fin
